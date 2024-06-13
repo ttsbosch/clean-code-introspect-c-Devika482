da654a463bf79d7603bf2341c840348aec21f7d6
Devika482 <devikamk@cet.ac.in>
2024-06-13 16:19:36 +0530

    Update converters.h

--- converters.h
+++ converters.h
@@ -1,1 +1,16 @@
-
+bool tryToConvertAsLongInt(const char* string, int* value) {
+    char* endptr;
+    *value = stringtol(string, &endptr, 10);
+    if (endptr == string) {
+        return 0;
+    }
+    return 1;
+}
+bool tryToConvertAsDouble(const char* string, double* value) {
+    char* endptr;
+    *value = stringtod(string, &endptr);
+    if (endptr == string) {
+        return 0;
+    }
+    return 1;
+}
